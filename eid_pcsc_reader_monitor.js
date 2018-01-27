10403590e0a9f06d2ce5aa99d6489575c011954d
Pedro Heliodoro <webmaster@phelios.org>
2018-01-27 14:30:04 +0000

    Completed procedure for address reading

---
+++ eid_pcsc_reader_monitor.js
@@ -0,0 +1,56 @@
+/**
+* Type: Hardware Monitor
+  Version: 0.0.1a
+  Description: Monitors the smartcard reader for EID presence
+**/
+
+var pcsclite = require('pcsclite');
+var pcsc = pcsclite();
+const shell = require('shelljs');
+var fs = require('fs');
+let CORE = require('./dependencies/core');
+
+var reader;
+var EID_MAP = JSON.parse(fs.readFileSync('./schemas/gov/'+CORE.CORE_COUNTRY+'/eid-map.json',function(err) {}));
+
+
+function registerReader(onCardInserted, onCardRemoved) {
+    pcsc.on('reader', function(r) {         
+        reader = r;
+
+        r.on('status', function(status) {
+            var changes = this.state ^ status.state;
+            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
+                console.log("card removed"); /* card removed */
+                reader["card_present"] = false;
+                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                onCardRemoved();
+            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
+                console.log("card inserted"); /* card inserted */                
+                reader["card_present"] = true;
+                reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
+                onCardInserted();                
+            }
+        });
+    });
+}
+
+function getReader() {
+    return reader;
+}
+
+function readAddress() {
+    return new Promise(function(resolve, reject) {
+        shell.exec('pkcs15-tool --read-data-object \'Citizen Address Data\' --pin 0000  --output /dev/shm/cc_address.hex', function(code, stdout, stderr) {            
+            fs.readFile('/dev/shm/cc_address.hex', function (err, data) {
+                if (err) reject(err);                
+                resolve(CORE.CORE_MAP_HEX_TO_JSON(data,EID_MAP.CITIZEN_ADDRESS));
+            });                    
+        });        
+    });
+}
+
+
+module.exports.getReader = getReader;
+module.exports.registerReader = registerReader;
+module.exports.readAddress = readAddress;
