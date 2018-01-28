63a21582628f1a6421ac3f4fddcb5a28b0131a76
Pedro Heliodoro <webmaster@phelios.org>
2018-01-28 10:50:26 +0000

    Dropped card present status in the monitor

--- eid_pcsc_reader_monitor.js
+++ eid_pcsc_reader_monitor.js
@@ -20,14 +20,10 @@ function registerReader(onCardInserted, onCardRemoved) {
 
         r.on('status', function(status) {
             var changes = this.state ^ status.state;
-            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {
-                console.log("card removed"); /* card removed */
-                reader["card_present"] = false;
+            if ((changes & this.SCARD_STATE_EMPTY) && (status.state & this.SCARD_STATE_EMPTY)) {                
                 reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
                 onCardRemoved();
-            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {
-                console.log("card inserted"); /* card inserted */                
-                reader["card_present"] = true;
+            } else if ((changes & this.SCARD_STATE_PRESENT) && (status.state & this.SCARD_STATE_PRESENT)) {            
                 reader.disconnect(reader.SCARD_LEAVE_CARD, function(err) {});
                 onCardInserted();                
             }
@@ -38,6 +34,18 @@ function registerReader(onCardInserted, onCardRemoved) {
 function getReader() {
     return reader;
 }
+
+function readIdentity(pin) {
+    return new Promise(function(resolve, reject) {
+        shell.exec('pkcs15-tool --read-data-object \'Citizen Data\' --pin '+pin+'  --output /dev/shm/cc_ident.hex', function(code, stdout, stderr) {            
+            fs.readFile('/dev/shm/cc_ident.hex', function (err, data) {
+                if (err) reject(err);                
+                resolve(CORE.CORE_MAP_HEX_TO_JSON(data,EID_MAP.CITIZEN_IDENTIF));
+            });                    
+        });        
+    });
+}
+
 
 function readAddress(pin) {
     return new Promise(function(resolve, reject) {
