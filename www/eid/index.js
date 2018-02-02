6240c355459986721d7d3183f9a8c0b594d1b3a0
Pedro Heliodoro <webmaster@phelios.org>
2018-02-02 19:39:57 +0000

    moved eid notification

--- www/eid/index.js
+++ www/eid/index.js
@@ -1,2 +1,13 @@
 "use strict";
-
+/* aditional trapping of the notifications in order to filter them */
+socket.on("notification", function(m){ 
+    if (m.notification =='eid_service') {
+        if (m.message == "CARD_INSERTED") {
+            $("#status").text("Card in");
+        }
+        if (m.message == "CARD_REMOVED") {
+            $("#status").text("Card out");
+        }        
+    }
+    
+})
